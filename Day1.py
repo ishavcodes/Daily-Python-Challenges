296e4e340e1e30adcc5eb1b78649bd1003cea81d
Isha Kumari <isha.v.codes@gmail.com>
2025-11-30 19:38:18 +0530

    Day 1 Python Code

---
+++ Day1.py
@@ -0,0 +1,63 @@
+""" Name = "Isha"
+Age = 22
+Learning = True
+print("Name :",Name)
+print("Age :",Age)
+print("Learning :",Learning) """
+
+
+
+# Type Casting ---------
+""" a = 45
+b = 1.23
+c = "Isha"
+d = False
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))  """
+
+# Task 1 ---------
+
+""" a = "50"
+b = int(a) + 20
+print(b) """
+
+# Task 2 -----------
+
+""" x = 45.67
+y = str(x)
+print(y)
+print(type(y)) """
+
+
+#Task 3 -------
+
+""" num = 0
+a = bool(num)
+print(a)
+ """
+
+# 1. int (Integer)
+
+""" num1 = 20 
+num2 = 30 
+print(num1 + num2) """
+
+#2. float (Decimal number)
+
+""" price = 99.99
+print(int(price)) """
+
+
+#3. str (String)
+
+""" name = "Python"
+print(len(name)) """
+
+#4. bool (Boolean)
+
+""" x = 10
+y = 20
+print(x < y)
+ """
